17738cce4fc3f668ea16556dde760a5ae4cabaac
oliviasmith4828-code <olivia.smith4828@gmail.com>
2026-07-20 14:51:18 +0530

    Update flow.py

--- flow.py
+++ flow.py
@@ -6,12 +6,21 @@ from playwright.sync_api import Playwright, sync_playwright
 
 def run(playwright: Playwright) -> None:
     # -------- AUTO SET YESTERDAY DATE --------
-    yesterday = datetime.now() - timedelta(days=1)
-    start_date = yesterday.strftime("%Y-%m-%d")
-    end_date = yesterday.strftime("%Y-%m-%d")
+ #   yesterday = datetime.now() - timedelta(days=1)
+  #  start_date = yesterday.strftime("%Y-%m-%d")
+   # end_date = yesterday.strftime("%Y-%m-%d")
 
 
 
+
+    # -------- FIXED DATE --------
+start_date = "2026-07-17"
+end_date = "2026-07-17"
+
+
+
+
+    
 
     
     browser = playwright.chromium.launch(headless=False)
